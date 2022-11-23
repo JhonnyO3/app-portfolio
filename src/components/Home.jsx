117bbf979e753c6448fd7882b0f0aa7b5a43c845
JhonnyO3 <jhonatas2004@gmail.com>
2022-11-23 18:27:49 -0300

    Ajustando responsividade

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,29 +1,41 @@
 import React from "react";
+import { DivHome } from "../style/styled";
+import { IoIosArrowDropdownCircle } from 'react-icons/io';
+import desktop from "../img/desktop.png"
 
 export default function Home() {
     return (
-        <main id="main">
-            <div id="container">
-                <div id="introducao">
-                    <h1>Sou o Jhonatas, desenvolvedor Full-Stack</h1>
+        <DivHome>
+            <main id="main">
+                <div id="container">
+                    <div id="introducao">
+                        <span id="linha"></span>
+                        <h1>Sou o Jhonatas,<br /> desenvolvedor Full-Stack</h1>
 
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam illo, cum aperiam mollitia adipisci voluptas sunt nam repudiandae itaque ratione consequuntur temporibus nostrum consequatur, vel ipsam sit repellendus velit! Ipsa?</p>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam illo, cum aperiam mollitia adipisci voluptas sunt nam repudiandae itaque ratione consequuntur temporibus nostrum consequatur, vel ipsam sit repellendus velit! Ipsa?</p>
+                        <h1 id="arrow-down"><IoIosArrowDropdownCircle id="icon" /></h1>
+                    </div>
+                    <div id="image">
+                        <img src={desktop} alt="" />
+                    </div>
                 </div>
                 <div id="about-work">
                     <div id="about">
-                    <h3>Sobre mim</h3>
-                    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Necessitatibus harum explicabo ea nemo, atque praesentium aperiam optio ex aspernatur, officia nisi eaque doloribus in deserunt nesciunt nihil, assumenda quam qui.</p>
+                        <h3>Sobre mim</h3>
+                        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Necessitatibus harum explicabo ea nemo, atque praesentium aperiam optio ex aspernatur, officia nisi eaque doloribus in deserunt nesciunt nihil, assumenda quam qui.</p>
+                        <a href="">Conheça mais sobre mim  </a>
                     </div>
                     <div id="work">
                         <h3>Meu trabalho</h3>
                         <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eius, est nam praesentium distinctio itaque inventore ratione veniam quos aliquid quidem reprehenderit dignissimos, possimus ipsum? Officia tenetur dicta culpa nam et.</p>
+                        <a href="">Venha conferir meu trabalho  </a>
                     </div>
+
+
                 </div>
 
+            </main>
+        </DivHome>
 
-            </div>
-
-        </main>
-        
     )
 }
--- src/style/styled.jsx
+++ src/style/styled.jsx
@@ -2,7 +2,166 @@ import React from "react";
 import styled from "styled-components";
 
 
+/*HOME-------------------------------------- */
+export const DivHome = styled.div`
+margin: 0;
+padding: 0;
+font-family: 'Roboto Mono', monospace;
 
+
+#container{
+  @media (max-width: 1920px) {
+    #main{
+      position: relative;
+      top: 10rem;
+    }
+  }
+  @media (max-width: 600px) {
+  flex-wrap: wrap;
+
+  img{
+    position: relative;
+    top: 3rem;
+    max-width: 20rem;
+  }
+
+
+  
+
+  #main {
+    position: relative;
+  }
+  #introducao {
+    max-width: 600px;
+
+
+    h1{
+      width: 100%;
+      
+      font-size: 2rem;
+    }
+    p{
+      font-size: 1.5rem;
+      width: 100%;
+    }
+
+  }
+  #introducao #arrow-down{
+    display: none;
+  }
+
+}
+}
+
+
+
+#arrow-down{
+  position: relative;
+  top: 2rem;
+  color: blue;
+
+  #icon {
+    font-size:12rem;
+    color: #7281F2;
+  }
+  #icon:hover{
+    transition: 2s;
+    color: #354ae9;
+    font-size: 13rem;
+  }
+ 
+
+  
+}
+
+#linha {
+  font-size: 5px;
+
+  padding-right:30%;
+  width: 100%;
+  position: relative;
+  bottom: 1rem;
+  background-color: white;
+}
+
+
+#main {
+  margin: auto;
+  width: 90%;
+  top: 10rem;
+  position: relative;
+
+
+  
+}
+
+#container {
+  background: none;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+
+}
+#introducao {
+  position: relative;
+  color: white;
+  max-width: 90%;
+  h1 {
+    font-weight: 900;
+    width: 100%;
+    font-size:4.8rem;
+  }
+  p{
+    word-break: break-all;
+    font-size: 2rem;
+    margin-top:2rem;
+    width: 85%;
+    text-align: justify;
+  }
+}
+#image{
+ img{
+  width: 30rem;
+  height: 100%;
+  border-radius: 50%;
+  background-color: #1a1a1a;
+ }
+
+}
+#about-work{
+  position: relative;
+  color: white;
+  display: flex;
+  flex-direction: column;
+  gap: 5rem;
+  top: 3rem;
+  padding-bottom: 10rem;
+
+  h3 {
+    font-size: 3.7rem;
+    font-weight: bold;
+  }
+  p{
+    font-size: 2rem;
+    margin-top: .5rem;
+    margin-bottom: 1rem;
+  }
+  a{
+    color: #7281F2;
+    margin-top: 5px;
+    font-size: 2rem;
+  }
+  a:hover {
+    transition: 2s;
+    color: #354ae9;
+    font-size: 3rem;
+  }
+}
+
+`
+
+/*HEADER------------------------------------ */
 export const DivHeader = styled.header`
 margin: 0;
 padding: 0;
@@ -15,12 +174,14 @@ a{
     text-decoration: none;
     font-family: 'Roboto Mono', monospace;
     font-size: 1.5rem;
+    padding: 10px;
 }
 a:hover {
     color: #7281f2;
     display: block;
 }
 #logo {
+    padding: 30px;
     font-size: 2rem;
     font-weight: bold;
     margin-left: 1rem;
@@ -34,6 +195,7 @@ a:hover {
     justify-content: space-between;
 }
 #menu {
+    padding: 20px;
     display: flex;
     list-style: none;
     gap: .5rem;
@@ -53,12 +215,14 @@ a:hover {
 
 @media (max-width: 600px) {
     #logo{
-        font-size: 1.3rem;
+        font-size: 1.7rem;
     }
     #btn-mobile {
         display: flex;
+        width: 100%;
+        height: 100%;
         padding: .5rem 1rem;
-        font-size: 1rem;
+        font-size: 2rem;
         border: none;
         background: none;
         cursor: pointer;
